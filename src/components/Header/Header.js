ebe065cfad573cf338e3011e010bdb969163b3c8
sanaAB <sanaabohelal@gmail.com>
2021-05-29 15:39:16 +0200

    Display recipes and router

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css"
+
+export default function header(){
+return( 
+        <section className="section2">
+            <div className="intro-image bg-image"></div>
+            <div className="intro_title bg-text">
+            <h1><b>Learn to cook your favorite middle eastern meals!</b></h1>
+            <h4>
+                Pick a recepie according to your diet and preference
+            </h4>
+            </div>
+        </section>);
+}
