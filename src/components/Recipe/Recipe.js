a0ef4fc17f9ded6658d0724da14f4996a198ea84
Lasoli <l.lipke@yahoo.com>
2021-06-05 19:34:29 +0200

    solved RecipePage problem

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -1,27 +1,22 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useParams
-} from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import "./Recipe.css";
-import RecipePage from"../RecipePage/RecipePage"
 
 export default function Recipe(props) {
-  const{category} = useParams();
-  console.log(props);
+  const { category } = useParams();
+  //  console.log(props.recipesArray);
   return (
     <div className="post">
-      {
-      props.recipesArray.filter((recipe)=>(recipe.fields.categoryMeal === category.toUpperCase())
-      ).map((recipe) => (
-        <Link to={recipe.sys.id}>{recipe.fields.name}
-          <h2>{ recipe.name }</h2>
-        </Link>
-          
-      ))}
+      {props.recipesArray
+        .filter(
+          (recipe) => recipe.fields.categoryMeal === category.toUpperCase()
+        )
+        .map((recipe) => (
+          <Link to={`/${category}/${recipe.sys.id}`}>
+            {recipe.fields.name}
+            <h2>{recipe.name}</h2>
+          </Link>
+        ))}
     </div>
   );
 }
